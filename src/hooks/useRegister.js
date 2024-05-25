18e1cc5cb2706d7352629231aa8562773e56ee8a
Isaac Castillo <isaac.castillo62055@uped.edu.sv>
2024-05-25 16:11:06 -0600

    🐛 corrigiendo hook de registros envio de formulario

--- src/hooks/useRegister.js
+++ src/hooks/useRegister.js
@@ -2,35 +2,84 @@ import Messages from "../lib/messages";
 import PocketBaseInstance from "../lib/pocketbase";
 import { toast } from "react-toastify";
 import router from "../routes/router";
+import { useForm } from "react-hook-form";
+
 export default function useRegister() {
   const { handleMessages } = Messages();
   const pocketbase = PocketBaseInstance();
 
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-    const formdata = new FormData(e.target.form);
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
 
+  const validates = {
+    email: {
+      register: register("email", {
+        required: "El email es requerido",
+        pattern: {
+          value: /^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$/,
+          message: "El email no es valido",
+        },
+      }),
+    },
+
+    password: {
+      register: register("password", {
+        required: "La contraseña es requerida",
+        minLength: {
+          value: 10,
+          message: "Agregar al menos 10 caracteres",
+        },
+      }),
+    },
+
+    name: {
+      register: register("name", {
+        required: "El nombre es requerido",
+        minLength: {
+          value: 5,
+          message: "El nombre debe tener al menos 5 caracteres",
+        },
+
+      }),
+    },
+
+    phone: {
+      register: register("phone", {
+        required: "El numero de telefono es requerido",
+        pattern: {
+          value: /^[0-9]{4}-[0-9]{4}$/,
+          message: "El numero de telefono no es valido",
+        },
+      }),
+    },
+  };
+
+  const sendData = async (data) => {
+    console.log(data);
+
+ 
     const user = {
-      username: formdata.get("email").split("@")[0],
-      email: formdata.get("email"),
-      password: formdata.get("password"),
-      passwordConfirm: formdata.get("password"),
-      name: formdata.get("name"),
+      username: data.email.split("@")[0],
+      email: data.email,
+      password: data.password,
+      passwordConfirm: data.password,
+      name: data.name,
       emailVisibility: true,
-      number_phone: formdata.get("phone"),
+      number_phone: data.phone,
     };
 
     try {
       await pocketbase.collection("users").create(user);
       toast.success("Usuario creado correctamente");
-      await void e.target.form.reset();
-      router.navigate("/login");
+      await router.navigate("/login");
     } catch (error) {
-  
       const messages = handleMessages(error?.response?.data);
       toast.error(`Error al crear el usuario: ${messages}`);
-    }
+    } 
   };
 
-  return { handleSubmit };
+  return { sendData, handleSubmit, validates, errors};
 }
