baccb81d047ee360067db4d856f8ce69b160af67
Isaac Castillo <isaac.castillo62055@uped.edu.sv>
2024-05-25 16:13:15 -0600

    ✅ actualizando registro con el nuevo componente

--- src/views/Register.jsx
+++ src/views/Register.jsx
@@ -1,9 +1,9 @@
 import useRegister from "../hooks/useRegister";
+import TextField from "../components/TextField";
 export default function Register() {
- 
-  const { handleSubmit } = useRegister();
+  const { handleSubmit, validates, sendData, errors } = useRegister();
   return (
-    <div className="sm:h-dvh flex justify-center bg-gradient-to-r from-slate-100 via-slate-200 to-slate-200">
+    <div className="flex justify-center bg-gradient-to-r from-slate-100 via-slate-200 to-slate-200">
       <div className=" bg-slate-50 xl:w-7/12 w-full flex flex-col sm:flex-row m-6 sm:gap-10 items-center rounded-lg border border-slate-300">
         <div className="sm:w-[50%] w-full bg-customYellowOpacity h-full flex items-center justify-center">
           <img className="object-cover" src="./food.png" alt="profile" />
@@ -12,51 +12,48 @@ export default function Register() {
           <br />
           <h1 className="text-3xl font-bold">Registrate</h1>
           <br />
-          <form className="flex flex-col gap-4 p-2">
-            <label htmlFor="name">Nombre completo:</label>
-            <input
-              className="focus:ring-amber-500 focus:border-amber-500 rounded-full"
-              type="text"
-              id="name"
+          <form
+            onSubmit={handleSubmit(sendData, (error) => console.log(error))}
+            className="flex flex-col p-2"
+          >
+            <TextField
+              label="Nombre completo"
               name="name"
-              required
-              placeholder="July Thomson"
+              type="text"
+              register={validates?.name?.register}
+              error={errors}
             />
-            <label htmlFor="email">Correo electronico:</label>
-            <input
-              className="focus:ring-amber-500 focus:border-amber-500 rounded-full"
-              type="email"
-              id="email"
+
+            <TextField
+              label="Correo electronico"
               name="email"
-              required
-              placeholder="email@gmail.com"
+              type="email"
+              register={validates?.email?.register}
+              error={errors}
             />
-            <label htmlFor="password">Contraseña:</label>
-            <input
-              className="focus:ring-amber-500 focus:border-amber-500 rounded-full"
-              type="password"
-              id="password"
+
+            <TextField
+              label="Contraseña"
               name="password"
-              required
-              placeholder="Pharase of 10 "
+              type="password"
+              register={validates?.password?.register}
+              error={errors}
             />
-            <label htmlFor="phone">Numero de telefono:</label>
-            <input
-              className="focus:ring-amber-500 focus:border-amber-500 rounded-full"
-              type="tel"
-              id="phone"
+
+            <TextField
+              label="Telefono"
               name="phone"
-              required
-              placeholder="0000-0000"
+              type="text"
+              register={validates?.phone?.register}
+              error={errors}
             />
+
             <br />
-            <button
+            <input
               className="rounded-full bg-amber-500 ring-4 ring-amber-500 ring-opacity-50 text-white font-bold p-3"
               type="submit"
-              onClick={handleSubmit}
-            >
-              Registrarse
-            </button>
+              value="Registrarse"
+            />
           </form>
         </div>
       </div>
