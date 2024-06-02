4f59dff201d5cc3838818a8fff2c1319f1c54384
Isaac Castillo <isaac.castillo62055@uped.edu.sv>
2024-06-02 13:26:04 -0600

    🔥 removiendo appbar

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import './App.css';
 import 'react-toastify/dist/ReactToastify.css';
-import AppBar from './components/AppBar';
+
 import reportWebVitals from './reportWebVitals';
 import { RouterProvider } from 'react-router-dom';
 import { ToastContainer } from 'react-toastify';
@@ -12,7 +12,6 @@ import router from './routes/router';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <AppBar />
     <ToastContainer />
     <RouterProvider router={router} />
   </React.StrictMode>
